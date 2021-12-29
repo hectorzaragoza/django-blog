48d8fa56f86426a7353afaf863b7a802efbdea97
hectorzaragoza <zaragoza09@gmail.com>
2021-12-29 09:04:47 -0800

    updated templates to render db data passed in from views and fetched through models from db

--- blog/views.py
+++ blog/views.py
@@ -1,68 +1,41 @@
-from datetime import date
-from django.shortcuts import render
+# from datetime import date
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 # Create your views here.
 
-all_posts = [
-    {
-        "slug": "hike-in-the-mountains",
-        "image": "glacier.jpeg",
-        "author": "Hector",
-        "date": date(2021,7,21),
-        "title": "Mountain Biking",
-        "excerpt": "There's nothing like the rush and peace of a flow trail.",
-        "content": """
-            This is a multi-Line String
-            Easier to write and read
-            :)
-        """ 
-    },
-    {
-        "slug": "some-picture",
-        "image": "2019-02-13.png",
-        "author": "Hector",
-        "date": date(2021,7,21),
-        "title": "Climbing",
-        "excerpt": "There's nothing like the rush and peace of a flow trail.",
-        "content": """
-            This is a multi-Line String
-            Easier to write and read
-            :)
-        """ 
-    },
-    {
-        "slug": "some-picture",
-        "image": "propic.jpg",
-        "author": "Hector",
-        "date": date(2021,7,21),
-        "title": "Climbing",
-        "excerpt": "There's nothing like the rush and peace of a flow trail.",
-        "content": """
-            This is a multi-Line String
-            Easier to write and read
-            :)
-        """ 
-    }
-]
+# all_posts = [
+#  Dummy Data   
+# ]
 
-# Helper method
-def get_date(post):
-    return post['date']
+# # Helper method
+# def get_date(post):
+#     return post['date']
 
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
+    # using dummy data
+    # sorted_posts = sorted(all_posts, key=get_date)
+    # latest_posts = sorted_posts[-3:]
+    # return render(request, "blog/index.html", {
+    #     "posts": latest_posts
+    # })
+
+    # using db data
+    latest_posts = Post.objects.all().order_by("-date")[:3] # this line creates a sql query, doesnt use python to do this...no performance hit of loading all records then slicing 3
     return render(request, "blog/index.html", {
         "posts": latest_posts
     })
 
 def posts(request):
+    all_posts = Post.objects.all().order_by("-date")
     return render(request, "blog/all-posts.html", {
         "all_posts": all_posts
     })
 
 def post_detail(request, slug):
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    # identified_post = next(post for post in all_posts if post['slug'] == slug)
+    identified_post = get_object_or_404(Post, slug=slug)
     return render(request, "blog/post-detail.html", {
-        "post": identified_post
+        "post": identified_post,
+        "post_tags": identified_post.tags.all()
     })
