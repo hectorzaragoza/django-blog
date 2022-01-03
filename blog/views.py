6d898c47bad0004c8450cf3a8e83b5444086838d
hectorzaragoza <zaragoza09@gmail.com>
2022-01-03 14:22:39 -0600

    migrated to class based views

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 # from datetime import date
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 
@@ -12,7 +13,21 @@ from .models import Post
 # def get_date(post):
 #     return post['date']
 
-def starting_page(request):
+class StartingPageView(ListView):
+    template_name = "blog/index.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "posts"
+    
+    # Need to modify slice and order
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
+    
+
+# Converting to Class-based View
+# def starting_page(request):
     # using dummy data
     # sorted_posts = sorted(all_posts, key=get_date)
     # latest_posts = sorted_posts[-3:]
@@ -21,21 +36,36 @@ def starting_page(request):
     # })
 
     # using db data
-    latest_posts = Post.objects.all().order_by("-date")[:3] # this line creates a sql query, doesnt use python to do this...no performance hit of loading all records then slicing 3
-    return render(request, "blog/index.html", {
-        "posts": latest_posts
-    })
+    # latest_posts = Post.objects.all().order_by("-date")[:3] # this line creates a sql query, doesnt use python to do this...no performance hit of loading all records then slicing 3
+    # return render(request, "blog/index.html", {
+    #     "posts": latest_posts
+    # })
 
-def posts(request):
-    all_posts = Post.objects.all().order_by("-date")
-    return render(request, "blog/all-posts.html", {
-        "all_posts": all_posts
-    })
+class AllPostsView(ListView):
+    template_name = "blog/all-posts.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "all_posts"
 
-def post_detail(request, slug):
-    # identified_post = next(post for post in all_posts if post['slug'] == slug)
-    identified_post = get_object_or_404(Post, slug=slug)
-    return render(request, "blog/post-detail.html", {
-        "post": identified_post,
-        "post_tags": identified_post.tags.all()
-    })
+# def posts(request):
+#     all_posts = Post.objects.all().order_by("-date")
+#     return render(request, "blog/all-posts.html", {
+#         "all_posts": all_posts
+#     })
+
+class SinglePostView(DetailView):
+    template_name = "blog/post-detail.html"
+    model = Post
+    # Detail View by defaul looks for slug (from urls page)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["post_tags"] = self.object.tags.all()
+        return context
+
+# def post_detail(request, slug):
+#     # identified_post = next(post for post in all_posts if post['slug'] == slug)
+#     identified_post = get_object_or_404(Post, slug=slug)
+#     return render(request, "blog/post-detail.html", {
+#         "post": identified_post,
+#         "post_tags": identified_post.tags.all()
+#     })
