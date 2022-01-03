8166d36a4477416e45f06b30878e8bfeb8c3b6d1
hectorzaragoza <zaragoza09@gmail.com>
2022-01-03 14:54:20 -0600

    Added Comment Form Model, HTML, and CSS

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.views.generic import ListView, DetailView
-
+from .forms import CommentForm
 # Create your views here.
 
 # all_posts = [
@@ -60,6 +60,7 @@ class SinglePostView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["post_tags"] = self.object.tags.all()
+        context["comment_form"] = CommentForm()
         return context
 
 # def post_detail(request, slug):
