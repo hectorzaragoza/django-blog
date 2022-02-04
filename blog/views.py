fdf79dcfb5c0ab1015183454e758385b36720a29
hectorzaragoza <zaragoza09@gmail.com>
2022-02-04 16:25:18 -0600

    add comment to db

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 # from datetime import date
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
 from .forms import CommentForm
+from django.views import View
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 # all_posts = [
@@ -53,15 +56,35 @@ class AllPostsView(ListView):
 #         "all_posts": all_posts
 #     })
 
-class SinglePostView(DetailView):
-    template_name = "blog/post-detail.html"
-    model = Post
-    # Detail View by defaul looks for slug (from urls page)
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["post_tags"] = self.object.tags.all()
-        context["comment_form"] = CommentForm()
-        return context
+class SinglePostView(View):
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm()
+        }
+        return render(request, "blog/post-detail.html", context)
+
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+        
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            
+            return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+        
+        
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm()
+        }
+        return render(request, "blog/post-detail.html", context)
+
 
 # def post_detail(request, slug):
 #     # identified_post = next(post for post in all_posts if post['slug'] == slug)
